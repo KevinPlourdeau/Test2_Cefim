e974a6268ae168369e8210716ac687eb3499f01e
Kevin Plourdeau <kevin.plourdeau@gmail.com>
2025-05-13 15:01:12 +0200

    Upload via desktop
    small modifications

--- js/quiz/questions.js
+++ js/quiz/questions.js
@@ -4,31 +4,31 @@
 export const questions = [
     { 
         question: "Quel langage est principalement utilisé pour structurer une page web ?",
-        image: "",
+        image: "images/img_1.PNG",
         answers: ["CSS", "HTML", "JavaScript"], 
         correct: "HTML" 
     },
     { 
         question: "Quelle propriété CSS permet de changer la taille de police ?",
-        image: "",
-        answers: ["font-family", "font-size", "text-style"], 
+        image: "images/img_2.PNG",
+        answers: ["font-family", "font-size", "text-decoration"], 
         correct: "font-size" 
     },
     { 
         question: "Quelle méthode JavaScript permet de transformer une chaîne JSON en objet JavaScript ?",
-        image: "",
+        image: "images/img_3.PNG",
         answers: ["JSON.stringify()", "JSON.parse()", "JSON.object()"], 
         correct: "JSON.parse()" 
     },
     { 
-        question: "Comment sélectionne-t-on tous les paragraphes en CSS ?",
-        image: "",
+        question: "Quel est le bon sélecteur CSS pour appliquer un style à toutes les balises <p> ?",
+        image: "images/img_4.PNG",
         answers: ["#p", "p", ".p"], 
         correct: "p" 
     },
     { 
         question: "Quelle commande Git permet de récupérer les modifications depuis un dépôt distant ?",
-        image: "", 
+        image: "images/img_5.PNG", 
         answers: ["git push", "git commit", "git pull", "git init"], 
         correct: "git pull" 
     }
